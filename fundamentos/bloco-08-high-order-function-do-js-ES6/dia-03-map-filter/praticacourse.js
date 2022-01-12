1b90cf74ada474397edafdb1fcd09cbc0355415b
Hanna <hanna-lana@hotmail.com>
2022-01-11 22:34:19 -0300

    Prática Course

---
+++ fundamentos/bloco-08-high-order-function-do-js-ES6/dia-03-map-filter/praticacourse.js
@@ -0,0 +1,190 @@
+/* const objPeople = [
+    { name: 'José', age: 21 },
+    { name: 'Lucas', age: 19 },
+    { name: 'Maria', age: 16 },
+    { name: 'Gilberto', age: 18 },
+    { name: 'Vitor', age: 15 },
+  ];
+  
+  const verifyAgeDrive = (arrayOfPeople) => (
+    arrayOfPeople.filter((people) => (people.age < 18))
+  );
+  
+  console.log(verifyAgeDrive(objPeople));
+  // [ { name: 'Maria', age: 16 }, { name: 'Vitor', age: 15 } ]
+
+  /* objPeople = arrayOfPeople 
+  -------------------------------------------------------------------------*/ 
+
+/*  const arrayMyStudents = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const removeStudentByName = (name, listStudents) =>
+  listStudents.filter((student) => student !== name);
+  // Filtra todos os estudantes que não têm o nome 'Ricardo' e retorna um array com eles. Na prática, remove o Ricardo do array.
+  // !== não é
+const newListStudents = removeStudentByName('Ricardo', arrayMyStudents); //listStudents == arrayMyStudent
+console.log(newListStudents); // [ 'Maria', 'Manuela', 'Jorge', 'Wilson' ]
+-------------------------------------------------------------------------*/ 
+
+/* Map
+
+fazendo com for
+
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = [];
+  
+  for (let index = 0; index < persons.length; index += 1) {
+    fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+  }
+  
+  console.log(fullNames);
+
+
+  agora fazendo com map  
+  -------------------------------------------------------------------*/
+/* 
+  const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+-------------------------------------------------------------------------- */
+
+/* const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ] */
+
+/* --------------------------------------------------------------------------  */
+/* const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: '59' },
+        { name: 'Português', nota: '80' },
+        { name: 'Química', nota: '78' },
+        { name: 'Biologia', nota: '92' },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '76' },
+        { name: 'Português', nota: '90' },
+        { name: 'Química', nota: '70' },
+        { name: 'Biologia', nota: '80' },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '91' },
+        { name: 'Português', nota: '85' },
+        { name: 'Química', nota: '92' },
+        { name: 'Biologia', nota: '90' },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '70' },
+        { name: 'Português', nota: '70' },
+        { name: 'Química', nota: '60' },
+        { name: 'Biologia', nota: '50' },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: '80' },
+        { name: 'Português', nota: '82' },
+        { name: 'Química', nota: '79' },
+        { name: 'Biologia', nota: '75' },
+      ],
+    },
+  ];
+const allNameStudents = estudantes.filter((estudante) => (
+    estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+  
+  console.log(allNameStudents);
+
+  ------------------------------------------------------------------------------ */
+/* 
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const sumNumbers = numbers.reduce((result, number) => result + number); // O parâmetro `result` é o acumulador. Ele recebe, do `reduce`, o retorno da função a cada iteração.
+console.log(sumNumbers); // 113
+
+// Ou seja:
+
+const getSum = (result, number) => result + number;
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
+
+--------------------------------------------
+
+MAIS CLARO AGORA (REDUCE)
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 32 47 50 52 47 103
+  return result + number;
+};
+
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113 */
+------------------------------------------------------------------------------
+/* const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 0 32 47 50 52 47 103
+  return result + number;
+  };
+const sumNumbers = numbers.reduce(getSum, 0); // Parâmetro adicionado ao reduce: o "0"
+console.log(sumNumbers); // 113 */
